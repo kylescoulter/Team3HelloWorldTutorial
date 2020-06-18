4fa0c5a87fa9def43adfe907f8a07137012536b4
Kara Kalinowski <mukamuka777@aol.com>
2020-06-18 18:26:30 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World!";
+      return "Hello GitHub!";
     }
 
     public static void main(String[] args) {
